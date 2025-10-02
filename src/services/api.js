93e70bd0c094aee8e747e73c990350f8f4ca8be0
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-10-02 18:04:46 +0100

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // backend URL
+  baseURL: 'https://vibestream-backend.onrender.com/api', // backend URL
 });
 
 // Add JWT token to headers if exists
